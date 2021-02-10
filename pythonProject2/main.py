359c38d8f81ef9e72cf608c2ea20d816be33700a
ghaziirfan123 <70496821+ghaziirfan123@users.noreply.github.com>
2021-02-10 10:57:46 -0600

    Add files via upload

---
+++ pythonProject2/main.py
@@ -0,0 +1,6 @@
+# My name is Mohammad Irfan and my peoplesoft is 1626488
+userNum = input()
+if userNum == '':
+    userNum = 0
+userNumSquared = int(userNum) * int(userNum)
+print(userNumSquared)
---
+++ pythonProject3/main.py
@@ -0,0 +1,10 @@
+# My name is Mohammad Irfan and my peoplesoft is 1626488
+user_num=int(input("Enter integer:\n"))
+squared=user_num*user_num
+cubed=squared*user_num
+print("You entered:",user_num)
+print(user_num,"squared is",squared)
+print("And",user_num,"cubed is",cubed,"!!")
+user_num2=int(input("Enter another integer:\n"))
+print(user_num,"+",user_num2,"is",user_num+user_num2)
+print(user_num,"*",user_num2,"is",user_num*user_num2)
